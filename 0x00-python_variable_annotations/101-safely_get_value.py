d8e228715e005281f9b82c7902de7eb21da6e24b
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-06 23:56:47 +0300

    Refactor safely_get_value function to use TypeVar for default parameter

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,13 +4,14 @@ that safely retrieves value from a dictionary.
 """
 
 from typing import Union, Any, Mapping, TypeVar
+T = TypeVar('T')
 
 
 def safely_get_value(
-                    dct: Mapping[Any, Union[Any, TypeVar('T')]],
+                    dct: Mapping[Any, Union[Any, T]],
                     key: Any,
-                    default: Union[TypeVar('T'), None] = None
-                    ) -> Union[Any, TypeVar('T')]:
+                    default: Union[T, None] = None
+                    ) -> Union[Any, T]:
     """ Return the value linked to key in dct,
     or default if key doesn't exist. """
     if key in dct:
