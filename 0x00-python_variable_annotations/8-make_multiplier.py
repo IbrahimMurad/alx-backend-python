b29cd0320c2bc4f0d9b6bc63644a77277d587ea7
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-06 22:50:45 +0300

    pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,7 @@ returns a function that multiplies a float by multiplier.
 """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """takes a float as argument and
 returns a function that multiplies a float by multiplier.
