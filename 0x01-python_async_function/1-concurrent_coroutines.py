45987791c4fc79803b2837cd45fc60957bae0331
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-11 00:53:16 +0300

    1- style fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """ spawns wait_random n times with the specified max_delay.
     and returns the list of all the delays (float values).
     The list of the delays should be in ascending order """
-    delay_list = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    delay_list = await asyncio.gather(
+        *[wait_random(max_delay) for _ in range(n)]
+        )
     return delay_list
