d6ac048c4e84f05f8459a5aa13a5b1287e1ac421
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-11 18:58:10 +0300

    all in one

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" measuring runtime
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ collect 10 random numbers using an async comprehension
+    over async_generator, then return the 10 random numbers. """
+
+    start = time.perf_counter()
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+        )
+    runTime = time.perf_counter() - start
+    return runTime
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,3 @@
+# Python Async Comprehension
+
+This project introduces async comprehension in python
