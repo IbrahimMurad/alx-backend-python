15ebb05ad28ebd9f43a1cc72e82a699d470d3a0e
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-07-01 13:51:57 +0300

    repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 from client import GithubOrgClient
 from parameterized import parameterized  # type: ignore
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from unittest import TestCase
 
 
@@ -23,3 +23,19 @@ class TestGithubOrgClient(TestCase):
             f"https://api.github.com/orgs/{org_name}"
         )
         self.assertEqual(response, expected)
+
+    @parameterized.expand([
+        ("google", {'repos_url': "https://api.github.com/orgs/google"}),
+        ("abc", {'repos_url': "https://api.github.com/orgs/abc"})
+    ])
+    def test_public_repos_url(self, org_name, expected) -> None:
+        """ test _public_repos_url method """
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock,
+        ) as mock_org:
+            mock_org.return_value = expected
+            test_class = GithubOrgClient(org_name)
+            payload_url = test_class._public_repos_url
+            mock_org.assert_called_once()
+            self.assertEqual(payload_url, expected['repos_url'])
