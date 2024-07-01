f5f0475b4e99f27fec070e2a08d6678be71e1ca8
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-07-01 13:03:20 +0300

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,19 +4,23 @@
 from client import GithubOrgClient
 from parameterized import parameterized
 from unittest.mock import patch
-from unittest import TestCase, main
+from unittest import TestCase
+from boto3.resources import response
 
 
 class TestGithubOrgClient(TestCase):
     """ test class for GithubOrgClient.org  """
     @parameterized.expand([
-        ("google"),
-        ("abc")
+        ("google", {'name': "google"}),
+        ("abc", {'name': "abc"})
     ])
-    @patch('requests.get')
-    def test_org(self, org_name, mock_get_json):
+    @patch('client.get_json')
+    def test_org(self, org_name, expected, mock_get_json) -> None:
         """ test org method """
+        mock_get_json.return_value = expected
         test_class = GithubOrgClient(org_name)
-        test_class.org()
-        mock_get_json.assert_called_once()
-    
+        response = test_class.org
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+        self.assertEqual(response, expected)
